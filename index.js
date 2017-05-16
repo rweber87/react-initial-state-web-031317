338bea960897c6fed1da55d2590bc062a330a157
Rob Weber <rob.weber87@gmail.com>
2017-05-16 08:54:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const app = "I don't do much.";
+
+class ToggleButton extends React.Component {
+  constructor() {
+    super();
+ 
+    this.state = {
+      isEnabled: false
+    };
+  }
+ 
+  render() {
+    return (
+      <div className="toggle-button">I am toggled {this.state.isEnabled ? 'on' : 'off'}.</div>
+    );
+  }
+}
